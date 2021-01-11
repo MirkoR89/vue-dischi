1e27e680631b2c51fa5fdf8e89df73473a5b67b7
Mirko <mirko.rinelli@gmail.com>
2021-01-11 15:41:11 +0100

    Linked albumApi in VueJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-	//Code
-});
+const app = new Vue({
+  el: '#app',
+  data: {
+    albumList: []
+  },
+  mounted(){
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(resp => {
+      console.log(resp.data.response);
+      albumApi = resp.data.response;
+      this.albumList.push(albumApi)
+    })
+  }
+})
