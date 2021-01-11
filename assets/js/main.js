b21f86a4c4f347432e44356b0acd98b16b1cee27
Mirko <mirko.rinelli@gmail.com>
2021-01-11 16:44:58 +0100

    V-for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 const app = new Vue({
   el: '#app',
   data: {
-    albumList: [1, 2, 3, 4]
+    albumList: []
   },
   mounted() {
     axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then(resp => {
         console.log(resp.data.response);
-        albumApi = resp.data.response;
-        this.albumList.push(albumApi);
+        this.albumList = resp.data.response
       })
   }
 })
